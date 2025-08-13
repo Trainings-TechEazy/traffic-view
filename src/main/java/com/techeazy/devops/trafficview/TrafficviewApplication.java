96fba1af3d0278372680bf4440f0590d5bd6620d
Chandan Pandey <cpandey05@gmail.com>
2025-08-13 21:16:48 +0530

    Enhaced comment

--- src/main/java/com/techeazy/devops/trafficview/TrafficviewApplication.java
+++ src/main/java/com/techeazy/devops/trafficview/TrafficviewApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrafficviewApplication {
 
-	//Main app file
+	//Main app file changes in feature branch
 	public static void main(String[] args) {
 		SpringApplication.run(TrafficviewApplication.class, args);
 	}
