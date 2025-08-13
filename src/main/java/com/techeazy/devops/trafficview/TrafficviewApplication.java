0bd215bc375a2ff897665270d2b41692f7332043
Chandan Pandey <cpandey05@gmail.com>
2025-08-13 19:44:11 +0530

    Initial version

---
+++ src/main/java/com/techeazy/devops/trafficview/TrafficviewApplication.java
@@ -0,0 +1,13 @@
+package com.techeazy.devops.trafficview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techeazy/devops/trafficview/TrafficviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeazy.devops.trafficview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
